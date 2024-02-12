94349a216ce8c8e48251e2d831a57191c4b3a8a9
Shakibul Islam <8080.shakib@gmail.com>
2024-02-12 23:07:13 +0600

    Hero section design complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,21 @@
-import React from "react"
-import "./hero.scss"
+import React from 'react'
+import './hero.scss'
 
 export default function Hero() {
   return (
-    <section className="hero container">
-      <p className="hero-trend">New trend</p>
-      <h1 className="hero__title">
-        <span className="hero__title-1">SUMMER SALE STYLE STYLISH</span>
-        <span className="hero__title-2">WOMENS</span>
-      </h1>
-      <a className="hero-link" href="#">
-        Discover More
-      </a>
-    </section>
+    <div className="container">
+      <section className="hero">
+        <p className="hero__trend">New trend</p>
+        <h1 className="hero__title">
+          <span>SUMMER SALE STYLE STYLISH</span>
+          <span>WOMENS</span>
+        </h1>
+        <a className="hero__link" href="#">
+          Discover More
+        </a>
+
+        <p className="hero__trend hero__scroll">Scroll</p>
+      </section>
+    </div>
   )
 }
